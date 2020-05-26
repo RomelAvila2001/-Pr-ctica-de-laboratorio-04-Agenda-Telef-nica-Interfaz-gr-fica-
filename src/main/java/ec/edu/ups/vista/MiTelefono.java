034091ede183ae930130e32ce6194ed95b8d505e
Romel_Avila <NANCY@DESKTOP-1JG1RL0>
2020-05-26 16:09:16 -0500

    actualizacion del codigo

--- src/main/java/ec/edu/ups/vista/MiTelefono.java
+++ src/main/java/ec/edu/ups/vista/MiTelefono.java
@@ -5,10 +5,51 @@
  */
 package ec.edu.ups.vista;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+
 /**
  *
  * @author NANCY
  */
-public class MiTelefono {
+public class MiTelefono extends Frame {
     
+    public MiTelefono()
+    {
+        this.setTitle("Agenda telefonica");
+        this.setSize(600, 400);
+        this.setLocation(10,10);
+        
+        BorderLayout border= new BorderLayout();
+        GridLayout grid= new GridLayout(5,1);
+        
+        Panel panelPrincipal= new Panel(border);
+        Panel panelCentral= new Panel(grid);
+        
+        Button boton1 = new Button("Registrar");
+        Button boton2 = new Button("Modificar");
+        Button boton3 = new Button("Eliminar");
+        Button boton4 = new Button("Buscar");
+        Button boton5 = new Button("Listar sus Telefonos");
+        Button boton6 = new Button("Salir");
+        
+        panelCentral.add(boton1);
+        panelCentral.add(boton2);
+        panelCentral.add(boton3);
+        panelCentral.add(boton4);
+        panelCentral.add(boton5);
+        
+        panelPrincipal.add(new Label("Bienvenido Usuario", Label.CENTER), BorderLayout.NORTH);
+        panelPrincipal.add(panelCentral, BorderLayout.CENTER);
+        panelPrincipal.add(boton6, BorderLayout.SOUTH);
+        
+        
+        this.add(panelPrincipal);
+        
+        this.setVisible(true);
+    }
 }
